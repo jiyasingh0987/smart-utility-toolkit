c96caca9aeb5c945e34bac4241e41759351144c0
jiyasingh0987 <jiyasingh8383@gmail.com>
2026-08-29 12:08:04 +0530

    Added execution flow logging

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+// const isEven = require("./modules/isEven");
+
+// console.log("Is 10 even?", isEven(10));
+// console.log("Is 7 even?", isEven(7));
 const isEven = require("./modules/isEven");
+const log = require("./modules/logger");
+
+log("Program started");
 
 console.log("Is 10 even?", isEven(10));
 console.log("Is 7 even?", isEven(7));
+
+log("Program completed");
